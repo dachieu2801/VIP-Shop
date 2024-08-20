bb52d546b78d69991c26a418ffd4e503ae4e045a
dachieu2801 <ankhieu322@gmail.com>
2024-08-20 10:49:59 +0700

    hieu

--- beike/Repositories/CategoryRepo.php
+++ beike/Repositories/CategoryRepo.php
@@ -57,16 +57,35 @@ class CategoryRepo
 
     public static function flatten(string $locale, $includeInactive = true, $separator = ' > '): array
     {
-        $sql = "SELECT cp.category_id AS id, TRIM(LOWER(GROUP_CONCAT(cd1.name ORDER BY cp.level SEPARATOR '{$separator}'))) AS name, c1.parent_id, c1.position";
-        $sql .= ' FROM category_paths cp';
-        $sql .= ' LEFT JOIN categories c1 ON (cp.category_id = c1.id)';
-        $sql .= ' LEFT JOIN categories c2 ON (cp.path_id = c2.id)';
-        $sql .= ' LEFT JOIN category_descriptions cd1 ON (cp.path_id = cd1.category_id)';
-        $sql .= " WHERE cd1.locale = '" . $locale . "' ";
+//        $sql = "SELECT cp.category_id AS id, TRIM(LOWER(GROUP_CONCAT(cd1.name ORDER BY cp.level SEPARATOR '{$separator}'))) AS name, c1.parent_id, c1.position";
+//        $sql .= ' FROM category_paths cp';
+//        $sql .= ' LEFT JOIN categories c1 ON (cp.category_id = c1.id)';
+//        $sql .= ' LEFT JOIN categories c2 ON (cp.path_id = c2.id)';
+//        $sql .= ' LEFT JOIN category_descriptions cd1 ON (cp.path_id = cd1.category_id)';
+//        $sql .= " WHERE cd1.locale = '" . $locale . "' ";
+//        if (! $includeInactive) {
+//            $sql .= ' AND c1.active = 1 ';
+//        }
+//        $sql .= ' GROUP BY cp.category_id ORDER BY c1.position ASC, name ASC';
+//
+//        return DB::select($sql);
+
+        $sql = "SELECT cp.category_id AS id,
+               TRIM(LOWER(GROUP_CONCAT(cd1.name ORDER BY cp.level SEPARATOR '{$separator}'))) AS name,
+               c1.parent_id,
+               c1.position
+        FROM category_paths cp
+        LEFT JOIN categories c1 ON (cp.category_id = c1.id)
+        LEFT JOIN categories c2 ON (cp.path_id = c2.id)
+        LEFT JOIN category_descriptions cd1 ON (cp.path_id = cd1.category_id)
+        WHERE cd1.locale = '" . $locale . "'";
+
         if (! $includeInactive) {
             $sql .= ' AND c1.active = 1 ';
         }
-        $sql .= ' GROUP BY cp.category_id ORDER BY c1.position ASC, name ASC';
+
+        $sql .= ' GROUP BY cp.category_id, c1.parent_id, c1.position
+          ORDER BY c1.position ASC, name ASC';
 
         return DB::select($sql);
     }
